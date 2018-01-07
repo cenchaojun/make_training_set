c8aa7769981b123e0ce72242b3c28b4cf2ab9d22
zxyle <zxyful@gmail.com>
2018-01-07 22:42:35 +0800

    print 改为pprint

--- use.py
+++ use.py
@@ -3,15 +3,17 @@
 # Author: Zheng <zxyful@gmail.com>
 
 import pickle
+from pprint import pprint
 from PIL import Image
 
 with open("./example/data_batch_1", 'rb') as file:
-    entry = pickle.load(file, encoding='latin1')
+    entry = pickle.load(file, encoding='utf-8')
     # print(entry)
     # print(entry.keys())
     # print(entry.get("filenames"))
     # print(entry.get("batch_label"))  # string
-    print(entry.get("data"))
+    # pprint(entry.get("data"))
+    pprint(entry)
     data = entry.get("data")[0].reshape(3, 70, 160).transpose([1, 2, 0])
-    print("标签: {}".format(entry.get("labels")[0]))
+    pprint("标签: {}".format(entry.get("labels")[0]))
     Image.fromarray(data, mode="RGB").show()
